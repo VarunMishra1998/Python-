1ebdf004570d4adbddef15e7668d29f1f82061c2
VarunMishra1998 <166588519+VarunMishra1998@users.noreply.github.com>
2026-01-14 14:24:06 +0530

    Add files via upload

---
+++ 1.0my_code.py
@@ -0,0 +1,6 @@
+print("hello varun")
+print("how are you")
+##print both in same line
+print("hello varun","how are you")
+##print varun in next line
+print("hello \nvarun") ##\n indicates next line 
---
+++ 1.1variable.py
@@ -0,0 +1,10 @@
+##variable is basically a container in which we stores the data
+name="varun"  ## _name="varun" underscore also can be use before the variable 
+age=35        #### _age=35 
+pi=3.14       ### ## _pi=3.14
+## _age=35,_name="varun",_pi=3.1 
+print(name,age,pi)
+##we can also print name in different way and can do operations on age
+print("my name is:",name)
+print("my age is:",age-5)
+##here name ,age ,pi is a variable and it is also a  identifiers
